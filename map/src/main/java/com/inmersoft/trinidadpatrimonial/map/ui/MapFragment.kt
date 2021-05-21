4d9b5198d23f4ad872031dbd618a4919e704967c
Joaquin Bordes Monroig <bordesmonroigj@gmail.com>
2021-05-20 23:53:50 -0400

    Add Map Fragment

---
+++ map/src/main/java/com/inmersoft/trinidadpatrimonial/map/ui/MapFragment.kt
@@ -0,0 +1,35 @@
+package com.inmersoft.trinidadpatrimonial.map.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.maps.android.ktx.awaitMap
+import com.inmersoft.trinidadpatrimonial.map.R
+import com.inmersoft.trinidadpatrimonial.map.databinding.MapFragmentBinding
+
+class MapFragment : Fragment() {
+    private var googleMap: GoogleMap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return MapFragmentBinding.inflate(inflater, container, false).also {
+            (childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment).also {
+                initMap(it)
+            }
+        }.root
+    }
+
+    private fun initMap(mapFragment: SupportMapFragment?) {
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            googleMap = mapFragment?.awaitMap()
+        }
+    }
+}
