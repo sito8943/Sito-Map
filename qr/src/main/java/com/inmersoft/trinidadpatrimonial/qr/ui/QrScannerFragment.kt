96a59451e8fbab617fddb2df3d91081eb228cd4a
Joaquin Bordes Monroig <bordesmonroigj@gmail.com>
2021-05-23 19:01:16 -0400

    Add Camera permission request and camera preview use case

--- qr/src/main/java/com/inmersoft/trinidadpatrimonial/qr/ui/QrScannerFragment.kt
+++ qr/src/main/java/com/inmersoft/trinidadpatrimonial/qr/ui/QrScannerFragment.kt
@@ -1,19 +1,80 @@
 package com.inmersoft.trinidadpatrimonial.qr.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.fragment.findNavController
+import com.google.common.util.concurrent.ListenableFuture
 import com.inmersoft.trinidadpatrimonial.qr.databinding.QrScannerFragmentBinding
 
 class QrScannerFragment : Fragment() {
+    private lateinit var cameraPreview: PreviewView
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return QrScannerFragmentBinding.inflate(inflater, container, false).root
+        val binding = QrScannerFragmentBinding.inflate(inflater, container, false)
+        cameraPreview = binding.cameraView
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        if (isPermissionGranted())
+            startCamera()
+        else
+            requestPermission.launch(REQUIRED_PERMISSION)
+
+        return binding.root
+    }
+
+    private fun startCamera() {
+        val fragmentContext = requireContext()
+        cameraProviderFuture = ProcessCameraProvider.getInstance(fragmentContext)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(fragmentContext))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder().build().apply {
+            setSurfaceProvider(cameraPreview.surfaceProvider)
+        }
+
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted)
+                startCamera()
+            else {
+                findNavController().popBackStack()
+            }
+        }
+
+    private fun isPermissionGranted() = ContextCompat.checkSelfPermission(
+        requireContext(), REQUIRED_PERMISSION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    companion object {
+        private const val TAG = "QrScannerFragment"
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
