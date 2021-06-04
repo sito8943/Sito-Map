9da0da43229db975d74838ec7755d012a7254f6d
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-06-04 17:40:58 -0400

    Agregado el Autocompletamiento a la hora de buscar en un lugar en el MapFragment.kt

--- app/src/main/java/com/inmersoft/trinidadpatrimonial/map/ui/MapFragment.kt
+++ app/src/main/java/com/inmersoft/trinidadpatrimonial/map/ui/MapFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -12,12 +14,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.textfield.TextInputLayout
 import com.inmersoft.trinidadpatrimonial.R
 import com.inmersoft.trinidadpatrimonial.core.imageloader.GlideImageLoader
 import com.inmersoft.trinidadpatrimonial.core.imageloader.ImageLoader
 import com.inmersoft.trinidadpatrimonial.databinding.MapFragmentBinding
 import com.inmersoft.trinidadpatrimonial.map.ui.adapter.PlaceTypeAdapter
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MapFragment : Fragment() {
@@ -28,7 +32,6 @@ class MapFragment : Fragment() {
     private lateinit var imageLoader: ImageLoader
 
     private lateinit var placesTypeAdapter: PlaceTypeAdapter
-
 
     private val mapFragmentViewModel: MapFragmentViewModel by viewModels()
 
@@ -59,6 +62,11 @@ class MapFragment : Fragment() {
             }
         }
 
+        val autoCompletePlacesNameAdapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line
+        )
+
         imageLoader = GlideImageLoader(requireContext())
 
         placesTypeAdapter =
@@ -68,6 +76,20 @@ class MapFragment : Fragment() {
 
         mapFragmentViewModel.allPlaceType.observe(viewLifecycleOwner, { placesTypeList ->
             placesTypeAdapter.setData(placesTypeList)
+        })
+
+        //AutoComplete
+
+        mapFragmentViewModel.allPlaces.observe(viewLifecycleOwner, {
+
+            val arrayPlacesName = it.map { place -> place.place_name }.toList()
+
+            autoCompletePlacesNameAdapter.addAll(arrayPlacesName)
+            val autoCompleteTextView: AutoCompleteTextView?
+            val textInputLayout: TextInputLayout = binding.searchField
+            autoCompleteTextView = textInputLayout.editText as AutoCompleteTextView?
+            autoCompleteTextView?.setAdapter(autoCompletePlacesNameAdapter)
+            autoCompletePlacesNameAdapter.notifyDataSetChanged()
         })
 
         return binding.root
