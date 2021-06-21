b96092c1f15f87a96aae88dd447f82640d1284c0
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-06-21 14:01:36 -0400

    Agregado los imconos de la pantalla de detalles de los lugares

--- app/src/main/java/com/inmersoft/trinidadpatrimonial/details/places/ui/fragments/PlaceDetailFragment.kt
+++ app/src/main/java/com/inmersoft/trinidadpatrimonial/details/places/ui/fragments/PlaceDetailFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.google.android.material.transition.MaterialContainerTransform
 import com.inmersoft.trinidadpatrimonial.R
 import com.inmersoft.trinidadpatrimonial.core.data.entity.Place
 import com.inmersoft.trinidadpatrimonial.databinding.PlaceDetailsFragmentBinding
@@ -27,8 +26,6 @@ class PlaceDetailFragment(private val placeData: Place) : Fragment() {
         binding = PlaceDetailsFragmentBinding.inflate(layoutInflater, container, false)
         binding.placeName.text = placeData.place_name
         binding.placeDescription.text = placeData.place_description
-
-
         Glide.with(requireContext())
             .load(
                 Uri.parse("$ASSETS_FOLDER/${placeData.header_images[0]}.jpg")
@@ -36,11 +33,6 @@ class PlaceDetailFragment(private val placeData: Place) : Fragment() {
             .error(R.drawable.placeholder_error)
             .placeholder(R.drawable.ic_placeholder)
             .into(binding.placeHeader)
-
-
         return binding.root
-
     }
-
-
 }
