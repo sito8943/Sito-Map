96d75d52a5cc24b19efae41428713a9f4be7ee7a
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-07-04 04:14:49 -0400

    Implementado In App Update

--- app/src/main/java/com/inmersoft/trinidadpatrimonial/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/inmersoft/trinidadpatrimonial/ui/onboarding/OnBoardingActivity.kt
@@ -11,12 +11,17 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.size
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.inmersoft.trinidadpatrimonial.R
 import com.inmersoft.trinidadpatrimonial.databinding.ActivityOnBoardingBinding
-import com.inmersoft.trinidadpatrimonial.ui.onboarding.data.OnBoardingData
 import com.inmersoft.trinidadpatrimonial.ui.onboarding.adapters.OnBoardingAdapter
 import com.inmersoft.trinidadpatrimonial.ui.onboarding.adapters.OnboardingViewPagerTransformer
+import com.inmersoft.trinidadpatrimonial.ui.onboarding.data.OnBoardingData
 import com.inmersoft.trinidadpatrimonial.ui.trinidad.TrinidadActivity
 import com.inmersoft.trinidadpatrimonial.utils.fadeTransition
 import com.inmersoft.trinidadpatrimonial.viewmodels.TrinidadDataViewModel
@@ -24,6 +29,12 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnBoardingActivity : AppCompatActivity() {
+
+    companion object {
+        private const val UPDATE_REQUEST_CODE = 7856
+    }
+
+    private val appUpdateManager by lazy { AppUpdateManagerFactory.create(this) }
 
     private val trinidadDataViewModel: TrinidadDataViewModel by viewModels()
 
@@ -55,19 +66,16 @@ class OnBoardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_TrinidadPatrimonial)
-
         window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
-
         // Attach a callback used to capture the shared elements from this Activity to be used
         // by the container transform transition
         setExitSharedElementCallback(MaterialContainerTransformSharedElementCallback())
-
         // Keep system bars (status bar, navigation bar) persistent throughout the transition.
         window.sharedElementsUseOverlay = false
-
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
 
+        inAppUpdate()
 
         binding.onboardingStartButton.setOnClickListener {
 
@@ -104,6 +112,49 @@ class OnBoardingActivity : AppCompatActivity() {
 
 
         setContentView(binding.root)
+    }
+
+    private fun inAppUpdate() {
+        appUpdateManager.registerListener {
+            if (it.installStatus() == InstallStatus.DOWNLOADED) {
+                showUpdateDownloadedSnackbar()
+            }
+        }
+
+        appUpdateManager.appUpdateInfo.addOnSuccessListener {
+            if (it.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && it.isUpdateTypeAllowed(
+                    AppUpdateType.FLEXIBLE
+                )
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    it,
+                    AppUpdateType.FLEXIBLE,
+                    this,
+                    UPDATE_REQUEST_CODE
+                )
+            }
+        }.addOnFailureListener {
+            Log.e("FlexibleUpdateActivity", "Failed to check for update: $it")
+        }
+    }
+
+    private fun showUpdateDownloadedSnackbar() {
+        Snackbar.make(
+            binding.root,
+            resources.getString(R.string.update_downloaded),
+            Snackbar.LENGTH_INDEFINITE
+        )
+            .setAction(resources.getString(R.string.install_update)) { appUpdateManager.completeUpdate() }
+            .show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager.appUpdateInfo.addOnSuccessListener {
+            if (it.installStatus() == InstallStatus.DOWNLOADED) {
+                showUpdateDownloadedSnackbar()
+            }
+        }
     }
 
     private fun setOnboardingPoint(index: Int) {
